742105fa2dfa37dc7219584521a8809f151d5ced
sekharbandla <rajasekhar.bandla@reancloud.com>
2017-09-14 20:40:57 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: windows-failover-cluster-feature-install
+# Cookbook Name:: windows-failover-cluster-creation
 # Recipe:: default
 #
 # Copyright 2017, YOUR_COMPANY_NAME
@@ -9,32 +9,22 @@
 
 ################ Windows Failover Cluster Configuration ####################
 =begin
-powershell_script 'windows-failover-cluster-feature-install' do
+powershell_script 'WSFC-config2' do
   code <<-EOH
-  $instancePvtIP = ( Invoke-WebRequest ` -Uri http://169.254.169.254/latest/meta-data/local-ipv4 ).Content
-  $GatewayIP = (Get-WmiObject -Class Win32_IP4RouteTable | where { $_.destination -eq '0.0.0.0' -and $_.mask -eq '0.0.0.0'} | Sort-Object metric1 | select nexthop, metric1, interfaceindex).nexthop
-  Netsh int ipv4 add address "Ethernet" address=$instancePvtIP mask='#{node['windows-failover-cluster-feature-install']['ServerSubnetMask']}' gateway=$GatewayIP
-  Install-WindowsFeature -Name Failover-Clustering -IncludeManagementTools
-  Restart-Computer
+  Test-Cluster
+  New-Cluster -Name '#{node['windows-failover-cluster-creation']['NewClusterName']}' -Node  $env:computername -StaticAddress '#{node['windows-failover-cluster-creation']['secondaryIP']}'
   EOH
-  domain node['windows-failover-cluster-feature-install']['DomainName']
-  user node['windows-failover-cluster-feature-install']['DomainUserName']
-  password node['windows-failover-cluster-feature-install']['DomainUserPassword']
+  domain node['windows-failover-cluster-creation']['DomainName']
+  user node['windows-failover-cluster-creation']['DomainUserName']
+  password node['windows-failover-cluster-creation']['DomainUserPassword']
 end
 =end
 
-Chef::ReservedNames::Win32::Security.add_account_right( 'Administrators', 'SeAssignPrimaryTokenPrivilege')
-Chef::ReservedNames::Win32::Security.add_account_right(node['windows-failover-cluster-feature-install']['DomainUserName'], 'SeAssignPrimaryTokenPrivilege')
-
-powershell_script 'windows-failover-cluster' do
+powershell_script 'cluster-creation' do
   code <<-EOH
-  Install-WindowsFeature -Name Failover-Clustering -IncludeManagementTools
+  New-Cluster -Name '#{node['windows-failover-cluster-creation']['NewClusterName']}' -Node  $env:computername -StaticAddress '#{node['windows-failover-cluster-creation']['secondaryIP']}'
   EOH
-end
-
-
-powershell_script 'Restart-Computer' do
-  code <<-EOH
-  Restart-Computer
-  EOH
+  domain node['windows-failover-cluster-creation']['DomainName']
+  user node['windows-failover-cluster-creation']['DomainUserName']
+  password node['windows-failover-cluster-creation']['DomainUserPassword']
 end
