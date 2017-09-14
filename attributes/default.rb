2aa9b8008616a846864a980d910440705008c114
sekharbandla <rajasekhar.bandla@reancloud.com>
2017-09-14 20:39:42 +0530

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,11 +8,11 @@
 # Package uri for dnow
 
 #Change the default admin email
-#default["windows-failover-cluster-feature-install"]["secondaryIP"]  = '10.0.25.24'
-#default["windows-failover-cluster-feature-install"]["NewClusterName"]  = 'cluster10'
-default["windows-failover-cluster-feature-install"]["DomainUserName"]  = 'opis\administrator'
-#default["windows-failover-cluster-feature-install"]["DomainUserPassword"]  = 'reanadmin@123'
-#default["windows-failover-cluster-feature-install"]["ServerSubnetMask"]  = '255.255.248.0'
-#default["windows-failover-cluster-feature-install"]["DomainName"]  = 'opis.internal'
-#default["windows-failover-cluster-feature-install"]["DomainServerIP"]  = '10.0.25.152'
+default["windows-failover-cluster-creation"]["secondaryIP"]  = '10.0.25.24'
+default["windows-failover-cluster-creation"]["NewClusterName"]  = 'cluster10'
+default["windows-failover-cluster-creation"]["DomainUserName"]  = 'administrator'
+default["windows-failover-cluster-creation"]["DomainUserPassword"]  = 'reanadmin@123'
+default["windows-failover-cluster-creation"]["DomainName"]  = 'opis.internal'
+#default["windows-failover-cluster-creation"]["DomainServerIP"]  = '10.0.25.152'
+
 
